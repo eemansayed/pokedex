6f7826d2e53353ba53ba10462ce59ecefd3575e7
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-26 09:55:28 +0000

    add const called getPokemon

--- script.js
+++ script.js
@@ -22,3 +22,9 @@ const fetchPokemons = () => {
     await.getPokemon(i);
   }
 };
+
+const getPokemon = async (i) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+};
